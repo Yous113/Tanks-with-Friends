e2bf4b868a0a0e4d13cb640790d13623fdfe0748
Youssef <youssef.n6@gmail.com>
2018-11-21 19:04:15 +0100

    Update ShellExplosion.cs

--- Tanks(virker)/Assets/Scripts/Shell/ShellExplosion.cs
+++ Tanks(virker)/Assets/Scripts/Shell/ShellExplosion.cs
@@ -46,8 +46,7 @@ public class ShellExplosion : MonoBehaviour
         m_ExplosionParticles.Play ();
         m_ExplosionAudio.Play ();
 
-        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.duration);
-        Destroy(gameObject);
+       Destroy(gameObject);
     }
 
 
